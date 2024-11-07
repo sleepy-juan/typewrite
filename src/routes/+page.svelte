74089880ee83a738063e6e8441568c479c226d00
Juan Lee <juanlee@kaist.ac.kr>
2024-11-07 19:19:20 +0900

    test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,21 @@
-<h1>
-인턴/채용 목록
-</h1>
+<h1>로플래닛</h1>
 
-<h2>완성!</h2>
+<div>
+    <h3>A 로펌</h3>
+    <p>대상: <span class="tag">1학년</span><span class="tag">2학년</span></p>
+    <p>5p 내외의 자기소개서</p>
+    <p>2024. 11. 15. 오후 2시 마감</p>
+</div>
+
+<div>
+    <h3>B 로펌</h3>
+    <p>대상: <span class="tag">1학년</span></p>
+    <p>5p 내외의 자기소개서</p>
+    <p>2024. 11. 17. 오후 2시 마감</p>
+</div>
+
+<style>
+    .tag {
+        background-color: #0xffff00f0
+    }
+</style>
